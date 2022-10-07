ead71cfb06a8ac9c90f90a379e94f37e2a76de26
AleksandrTikka <alexandertikka@gmail.com>
2022-10-08 01:24:38 +0300

    TaskList получил задачи из свойства tasks и фильтры

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,18 @@
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+// Импортируем хук
+import { useSelector } from "react-redux";
+// Импортируем объект значений фильтра
+import { statusFilters } from "../../redux/constans";
 
 export const StatusFilter = () => {
+  // Получаем значение фильтра из состояния Redux
+  const filter = useSelector((state) => state.filters.status);
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
