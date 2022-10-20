f6ab3b13d0c20942f2373b4194d297b5b671c670
AleksandrTikka <alexandertikka@gmail.com>
2022-10-20 16:33:04 +0300

    actions

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -1,27 +1,30 @@
 import css from "./TaskCounter.module.css";
 // Импортируем хук
-import { useSelector } from "react-redux";
-import { getTasks } from "../../redux/selectors";
+// import { useSelector } from "react-redux";
+// import { getTasks } from "../../redux/selectors";
+import { useTaskCount } from "../../hooks/useTaskCount";
 
 export const TaskCounter = () => {
   // Получаем массив задач из состояния Redux
-  const tasks = useSelector(getTasks);
+  // const tasks = useSelector(getTasks);
   // На базе состояния Redux получаем производные данные
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { active: 0, completed: 0 }
-  );
+  // const count = tasks.reduce(
+  //   (acc, task) => {
+  //     if (task.completed) {
+  //       acc.completed += 1;
+  //     } else {
+  //       acc.active += 1;
+  //     }
+  //     return acc;
+  //   },
+  //   { active: 0, completed: 0 }
+  // );
+
+  const { active, completed } = useTaskCount();
   return (
     <div>
-      <p className={css.text}>Active: {count.active}</p>
-      <p className={css.text}>Completed: {count.completed}</p>
+      <p className={css.text}>Active: {active}</p>
+      <p className={css.text}>Completed: {completed}</p>
     </div>
   );
 };
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,28 +1,43 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Task } from "../Task/Task";
 import css from "./TaskList.module.css";
 // Импортируем объект значений фильтра
-import { statusFilters } from "../../redux/constans";
-import { getTasks, getStatusFilter } from "../../redux/selectors";
+// import { statusFilters } from "../../redux/constans";
+// import { getTasks, getStatusFilter } from "../../redux/selectors";
+import { useVisibleTasks } from "../../hooks/useVisibleTasks";
 
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter((task) => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter((task) => task.completed);
-    default:
-      return tasks;
-  }
-};
+// const useVisibleTasks = () => {
+//   const tasks = useSelector(getTasks);
+//   const filter = useSelector(getStatusFilter);
+//   switch (filter) {
+//     case statusFilters.active:
+//       return tasks.filter((task) => !task.completed);
+//     case statusFilters.completed:
+//       return tasks.filter((task) => task.completed);
+//     default:
+//       return tasks;
+//   }
+// };
+
+// const getVisibleTasks = (tasks, statusFilter) => {
+//   switch (statusFilter) {
+//     case statusFilters.active:
+//       return tasks.filter((task) => !task.completed);
+//     case statusFilters.completed:
+//       return tasks.filter((task) => task.completed);
+//     default:
+//       return tasks;
+//   }
+// };
 
 export const TaskList = () => {
   // Получаем массив задач из состояния Redux
-  const tasks = useSelector(getTasks);
+  // const tasks = useSelector(getTasks);
   // Получаем значение фильтра из состояния Redux
-  const statusFilter = useSelector(getStatusFilter);
+  // const filter = useSelector(getStatusFilter);
   // Вычисляем массив задач которые необходимо отображать в интерфейсе
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  // const visibleTasks = getVisibleTasks(tasks, filter);
+  const visibleTasks = useVisibleTasks();
 
   return (
     <ul className={css.list}>
